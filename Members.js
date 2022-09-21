7c2b65abef6ff394e4a666233130e9b0420c5eb4
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-20 21:16:13 -0400

    init

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'jdoe@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 2,
+    name: 'Sam Smith',
+    email: 'ssmith@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 3,
+    name: 'Miranda Doesnt',
+    email: 'mdoesnt@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 4,
+    name: 'Paul Salizan',
+    email: 'psalizan@gmail.com',
+    status: 'inactive',
+  },
+]
+
+module.exports = members
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const logger = require('./middleware/logger')
+const app = express()
+
+// init middleware
+// app.use(logger)
+
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/members', require('./routes/api/members'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
